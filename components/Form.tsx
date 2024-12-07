930af6792230fcb4ebea422ee5f1bffd955ec5a9
Volodymyr <babihuk888@gmail.com>
2024-12-07 15:21:40 +0200

    updated

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import React from 'react';
-import { cn } from '@/lib/utils';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 export function Form({ isSignUp }: { isSignUp: boolean }) {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Form submitted');
+    console.log("Form submitted");
   };
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black dark:shadow-glow">
@@ -40,7 +40,7 @@ export function Form({ isSignUp }: { isSignUp: boolean }) {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          {isSignUp ? 'Sign up ' : 'Sign in '}
+          {isSignUp ? "Sign up " : "Sign in "}
           &rarr;
           <BottomGradient />
         </button>
@@ -49,12 +49,12 @@ export function Form({ isSignUp }: { isSignUp: boolean }) {
 
         <div className="flex flex-col space-y-4">
           <span className="text-white dark:text-white text-sm text-center">
-            {isSignUp ? 'Already have an account?' : 'Don’t have an account?'}{' '}
+            {isSignUp ? "Already have an account?" : "Don’t have an account?"}{" "}
             <Link
               className="text-blueSecond dark:text-blueSecond underline font-bold"
-              href={isSignUp ? '/signin' : '/signup'}
+              href={isSignUp ? "/signin" : "/signup"}
             >
-              {isSignUp ? 'Sign in' : 'Sign up'}
+              {isSignUp ? "Sign in" : "Sign up"}
             </Link>
           </span>
         </div>
@@ -79,5 +79,9 @@ const LabelInputContainer = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  return <div className={cn('flex flex-col space-y-2 w-full', className)}>{children}</div>;
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
 };
