6ed1d9fd78cb2b623d698c540173010595392e7c
Volodymyr <babihuk888@gmail.com>
2024-12-11 21:32:19 +0200

    updated

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { redirect } from 'next/navigation';
-import { getTokenFromLocalStorage } from '@/utils/localStorage';
 import { toast } from 'react-toastify';
 
 export default function ProfileLayout({
@@ -9,11 +9,32 @@ export default function ProfileLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const token = getTokenFromLocalStorage();
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (!token) {
-    toast.error('You need to be logged in to access this page.');
-    redirect('/signin?error=You must log in to access this page.');
+  useEffect(() => {
+    const getTokenFromLocalStorage = (): string | null => {
+      if (typeof window === 'undefined') {
+        return null; // localStorage недоступний на сервері
+      }
+      return localStorage.getItem('token');
+    };
+
+    const token = getTokenFromLocalStorage();
+
+    if (!token) {
+      toast.error('You need to be logged in to access this page.');
+      redirect('/signin?error=You must log in to access this page.');
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
   }
 
   return <>{children}</>;
